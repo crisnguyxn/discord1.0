43f58677591fc3ca5750e587a8373f8da8e12a8a
crisnguyxn <hyundev1406@gmail.com>
2023-05-27 22:35:33 +0700

    add logics

--- backend/src/routes/discord-room.js
+++ backend/src/routes/discord-room.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const {createRoom, getVoiceRoom, postMessage, getMessages ,getChannel} = require('../controllers/discord-room')
+const {createRoom, getTextRoom, postMessage, getMessages ,getChannel} = require('../controllers/discord-room')
 const router = express.Router()
 
 router.route('/create').post(createRoom)
-router.route('/:id').get(getVoiceRoom)
+router.route('/:id').get(getTextRoom)
 router.route('/message').post(postMessage)
 router.route('/message/:id').get(getMessages)
 router.route('/channels/:id').get(getChannel)
